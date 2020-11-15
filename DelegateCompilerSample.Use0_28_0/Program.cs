97839de4ba666405db50fb8a9bbdb29445443fdd
rioka <rioka@ASUS-GL552>
2020-11-15 11:02:55 +0000

    Align code in Program.cs

--- DelegateCompilerSample.Use0_28_0/Program.cs
+++ DelegateCompilerSample.Use0_28_0/Program.cs
@@ -10,8 +10,20 @@ namespace DelegateCompilerSample.Use0_28_0
     {
       using (var ctx = new Context())
       {
+        Console.WriteLine(ctx.Database.Connection.ConnectionString);
+
         var orders = ctx.Orders.ToList();
         Console.WriteLine($"{orders.Count} order(s) found");
+
+        foreach (var order in orders)
+        {
+          Console.WriteLine($"{order.Id}");
+
+          foreach (var line in order.OrderLines)
+          {
+            Console.WriteLine($"\t{line.Id} {line.Product} {line.Qty}");
+          }
+        }
       }
     }
   }
