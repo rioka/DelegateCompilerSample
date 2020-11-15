96694933c0f958abd8d91a197f70f6414b702bd5
rioka <rioka@ASUS-GL552>
2020-11-15 11:02:37 +0000

    Add new sample project using DelegateDecompiler v0.28.2

---
+++ DelegateCompilerSample.Use0_28_2/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace DelegateCompilerSample.Use0_28_2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DelegateCompilerSample.EF.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DelegateCompilerSample.EF.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ DelegateCompilerSample.Use0_28_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using DelegateCompilerSample.EF;
+
+namespace DelegateCompilerSample.Use0_28_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      using (var ctx = new Context())
+      {
+        Console.WriteLine(ctx.Database.Connection.ConnectionString);
+
+        var orders = ctx.Orders.ToList();
+        Console.WriteLine($"{orders.Count} order(s) found");
+
+        foreach (var order in orders)
+        {
+          Console.WriteLine($"{order.Id}");
+
+          foreach (var line in order.OrderLines)
+          {
+            Console.WriteLine($"\t{line.Id} {line.Product} {line.Qty}");
+          }
+        }
+      }
+    }
+  }
+}
